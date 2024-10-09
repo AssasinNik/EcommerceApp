40b06d9b786f7455c840b525deaa60c6f7d6b61b
AssasinNik <nikitacherenkov228@gmail.com>
2024-10-09 16:16:18 +0300

    Add ui for Fragments

---
+++ app/src/main/java/com/nikitacherenkov/ecommerceapp/fragments/loginRegister/AccountOptionsFragment.kt
@@ -0,0 +1,7 @@
+package com.nikitacherenkov.ecommerceapp.fragments.loginRegister
+
+import androidx.fragment.app.Fragment
+import com.nikitacherenkov.ecommerceapp.R
+
+class AccountOptionsFragment: Fragment(R.layout.fragment_account_options) {
+}
